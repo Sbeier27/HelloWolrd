478b61a29c50c4a7af0005da6ec8af1bb724b954
Sbeier27 <s.beier27@ncf.edu>
2023-10-23 19:19:42 -0400

    Committing a crime

---
+++ main.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+print( "Hello, world!" )
+
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'rx')
+plt.ylabel('some numbers')
+plt.show()
+
+def plot_it(x,y):
+    for x less than 10:
+        x_coord = 10-x
+        x+1
+    for y less than 10:
+        y_coord = 0-y
+        y+1
+
+
+    plt.plot(x_coord, y_coord, 'kd')
+    plt.show()
+
+plot_it(0,0)
